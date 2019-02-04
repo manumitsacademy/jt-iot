f52e165bd1074b7426a85aad1256989e38d40e3b
praveengclasses <praveengclasses@gmail.com>
2019-02-04 22:35:23 +0530

    'togglebutton

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -11,20 +11,28 @@ export class HomeComponent {
   constructor(private http:HttpClient){}
   loginstatus=true;
   data;
+  macid;
   h;
   t;
   motorSwitch:boolean=true;
+  deviceStatusFlag;
   ngOnInit(){
-    setInterval(()=>{
-      this.http.get("http://ec2-52-66-255-20.ap-south-1.compute.amazonaws.com:4000/getDeviceData/0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED")
-      .subscribe((res)=>{
-        console.log(res);
-        this.data=res;
+    setInterval(this.getDeviceData.bind(this),3000);
+  }
+  getDeviceData(){
+    this.http.get("http://ec2-52-66-255-20.ap-south-1.compute.amazonaws.com:4000/getDeviceData/0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED")
+    .subscribe((res)=>{
+      console.log(res);
+      this.data=res;
+      this.deviceStatusFlag=res[0].deviceStatus;
+      if(res[0].deviceStatus==='connected'){      
         this.h=res[0]['h'].toFixed(2);
         this.t=res[0]['t'].toFixed(2);
+        this.motorSwitch=res[0]['LED'];
         console.log(this.h,this.t)
-      });
-    },3000);
+      }
+    });
+  
   }
   title = 'myapp';
   gaugeType = "semi";
@@ -33,7 +41,11 @@ export class HomeComponent {
   temperatureLabel = "Temperature";
   temperatureAppendText = "C";
   motorRestart(){
-    console.log(this.motorSwitch)
+    this.http.get(`http://ec2-52-66-255-20.ap-south-1.compute.amazonaws.com:5002/command/0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED/${this.motorSwitch}/gubbalapraveen`)
+      .subscribe((res)=>{
+        console.log(res)
+        setInterval(this.getDeviceData.bind(this),100);
+      });
   }
 
 }
